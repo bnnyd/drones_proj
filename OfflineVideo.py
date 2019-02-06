f7642ed25e12dc2dd657f525164720ed53f724da
or kutner <orki777@gmail.com>
2019-02-06 14:40:00 +0200

    add video

--- OfflineVideo.py
+++ OfflineVideo.py
@@ -2,46 +2,48 @@
 import cv2
 import os
 
-from imageProcessing import ImageProcessing
+from Control.imageProcessing import ImageProcessing
+
+videos = ['offline.mp4', 'offline1.mp4', 'offline2.mp4']
+
+for video in videos:
+    vidcap = cv2.VideoCapture(video)
+    success,image = vidcap.read()
+    rate = 25
+    cnt = 25
+    num = 0
+    while success:
 
 
-vidcap = cv2.VideoCapture('offline1.mp4')
-success,image = vidcap.read()
-rate = 25
-cnt = 25
-num = 0
-while success:
+        success,image2 = vidcap.read()
 
-
-    success,image2 = vidcap.read()
-
-    if cnt==rate:
-        image = image2
-        cX, cY, xlen, ylen, pr_img = ImageProcessing.object_center(image)
-        cnt = 0
+        if cnt==rate:
+            image = image2
+            cX, cY, xlen, ylen, pr_img = ImageProcessing.object_center(image)
+            cnt = 0
 
 
 
-    if cX == 2000:
-        cv2.putText(image, "Empty dir", (640, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    elif cX == 3000:
-        cv2.putText(image, "No objects", (640, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        if cnt == 0:
-            cv2.imwrite("no_obj_" + str(num) + ".jpg", pr_img)
-            num = num + 1
-    elif cX == 4000:
-        cv2.putText(image, "Too many objects", (640, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        if cnt == 0:
-            cv2.imwrite("too_many_" + str(num) + ".jpg", pr_img)
-            num = num + 1
-    else:
-        cX = int(cX)
-        cY = int(cY)
-        cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
-        cv2.putText(image, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    cv2.imshow("video", image)
-    cv2.waitKey(1)
-    cnt = cnt + 1
+        if cX == 2000:
+            cv2.putText(image, "Empty dir", (640, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        elif cX == 3000:
+            cv2.putText(image, "No objects", (640, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            if cnt == 0:
+                cv2.imwrite(video + "_no_obj_" + str(num) + ".jpg", pr_img)
+                num = num + 1
+        elif cX == 4000:
+            cv2.putText(image, "Too many objects", (640, 360), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            if cnt == 0:
+                cv2.imwrite(video + "_too_many_" + str(num) + ".jpg", pr_img)
+                num = num + 1
+        else:
+            cX = int(cX)
+            cY = int(cY)
+            cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
+            cv2.putText(image, "centroid", (cX - 25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.imshow(video, image)
+        cv2.waitKey(1)
+        cnt = cnt + 1
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
--- run_video_proc.csh
+++ run_video_proc.csh
@@ -1,7 +1,7 @@
 #!/usr/csh
 
-rm -f no_obj*
-rm -f too_many*
+rm -f *no_obj*
+rm -f *too_many*
 
-python3 imageProcessing.py
+#python3 imageProcessing.py
 python3 OfflineVideo.py
